85a95b39d058bc5e0e07a27f16171f759a877dbf
Cohen Schulz <cohenschulz1204@gmail.com>
2023-09-08 13:21:21 -0400

    init push

---
+++ flashcards/character/character.js
@@ -0,0 +1,23 @@
+let mainL = [ '我', '你', '他', '老师', '学生' ];
+let cont = false;
+
+function startDialogue(info) {
+    cont = false;
+
+}
+
+function beginSet() {
+    console.log("Beginning set");
+
+    // Remove the button and display the character animation for the characters in the gobal list
+    document.getElementById("beginButton").classList.add("hidden");
+
+    let info = document.getElementById("displayInfo");
+
+    if (!cont) {
+
+    }
+
+
+
+}
